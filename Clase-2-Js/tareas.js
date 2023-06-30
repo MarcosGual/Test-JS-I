b5b8957226437869aa3590ed63c8a05d1376413e
MarcosGual <marcos.gual@outlook.com>
2023-06-30 19:29:00 -0300

    add: primeros ejercicios resueltos

--- Clase-2-Js/tareas.js
+++ Clase-2-Js/tareas.js
@@ -9,16 +9,16 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 // Tip: borrar el dato 'null' antes de poner el dato tipo string.
-const nuevaString = null;
+const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 10.99;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - 5 === null;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === null ;
